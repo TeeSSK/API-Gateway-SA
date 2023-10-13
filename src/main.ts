f0857fbac5d53fac6aea0632c59fba8124c0f121
sukij <sukij44800@gmail.com>
2023-10-13 23:58:53 +0700

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors();
   app.use(cookieParser());
   await app.listen(3001);
 }
