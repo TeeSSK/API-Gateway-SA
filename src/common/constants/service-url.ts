dbea593f87b90f9e9fba4afd2bfcacfdc9406c5e
K. Jetnipit <jetnipit54@gmail.com>
2023-11-09 18:06:31 +0700

    chore: update constant var

--- src/common/constants/service-url.ts
+++ src/common/constants/service-url.ts
@@ -1,3 +1,3 @@
-export const SUBJECT_SERVICE_URL: string = 'localhost:8080';
-export const FILE_SERVICE_URL: string = 'localhost:8082';
-export const TOPIC_SERVICE_URL: string = 'http://localhost:4000';
+export const SUBJECT_SERVICE_URL: string = 'subject-app-service:8080';
+export const FILE_SERVICE_URL: string = 'filestorage-app-service:8000';
+export const TOPIC_SERVICE_URL: string = 'topic-backend:4000';
